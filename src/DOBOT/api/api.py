432109b834cf572ccf6900851c2726db1f81818c
nclaassen01 <96117474+nclaassen01@users.noreply.github.com>
2024-02-15 08:14:20 +0100

    Added dobot api
    This is version 1 of the dobot api. This is still WIP

---
+++ src/DOBOT/api/api.py
@@ -0,0 +1,50 @@
+from flask import Flask, jsonify, request
+# from flasgger import Swagger, LazyString, LazyJSONEncoder
+# from flasgger import swag_from
+from flask_swagger_ui import get_swaggerui_blueprint
+
+app = Flask(__name__)
+
+### swagger specific ###
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.config.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Seans-Python-Flask-REST-Boilerplate"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
+
+
+### api endpoints
+
+@app.route("/start", methods=['POST'])
+def startDoBot():
+    # Do the stuff you need to do
+    return jsonify(processResult)
+
+@app.route("/stop", methods=['POST'])
+def stopDoBot():
+    # Do the stuff you need to do
+    return jsonify(processResult), 200
+
+@app.route("/emergency-stop", methods=['POST'])
+def emergencyStop():
+    # Do the stuff you need to do
+    return jsonify(), 200
+
+@app.route("/position", methods=['POST'])
+def position():
+    # Do the stuff you need to do
+    return jsonify(), 200
+
+@app.route("/login", methods=['POST'])
+def login():
+    # Do the stuff you need to do
+    return jsonify(), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
