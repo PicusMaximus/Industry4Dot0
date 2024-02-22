4049124e9adb7a30fe3037289ca994705f3aff14
dinge <dinge@dinge.de>
2024-02-22 03:09:14 +0100

    SPS python db_write

--- src/SPS/Main.py
+++ src/SPS/Main.py
@@ -1,4 +1,11 @@
 import snap7
+
+dbnumber = 1
+rack = 0
+slot = 1
+
+
 client = snap7.client.Client()
-client.connect("192.168.0.1", 0, 1)
-print(client.db_read(2,0,1))
+client.connect("192.168.0.1", rack, slot)
+print(client.db_read(dbnumber,0,1))
+client.db_write(dbnumber,0,bytearray(b'\x01'))
