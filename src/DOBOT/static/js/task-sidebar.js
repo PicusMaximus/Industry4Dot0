47a8f73f04d29b98d9a69c3024462b5448448e14
nclaassen01 <96117474+nclaassen01@users.noreply.github.com>
2024-03-10 23:38:47 +0100

    fixed drag and drop issue

--- src/DOBOT/static/js/task-sidebar.js
+++ src/DOBOT/static/js/task-sidebar.js
@@ -34,7 +34,7 @@ $addMoveBtn.on('click', () => {
             </div>
             <div id="collapse-content-${moveCount}" class="collapse-content-container cursor-pointer hs-collapse hidden w-full overflow-hidden transition-[height] duration-300" aria-labelledby="hs-basic-collapse">
                 <div class="flex flex-col mt-2 ml-8 command-chain-container gap-x-1 gap-y-2 move-list--draggable">
-                    <button type="button" class="move-list-item--draggable flex gap-1 items-center pl-1 pr-1 text-sm text-slate-700 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                    <button id="something-random-1231231536815368" type="button" class="move-list-item--draggable flex gap-1 items-center pl-1 pr-1 text-sm text-slate-700 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
                         <svg xmlns="http://www.w3.org/2000/svg" class="flex-shrink-0 size-5 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecapspic="round" stroke-linejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -50,6 +50,12 @@ $addMoveBtn.on('click', () => {
 
     const collapse = new HSCollapse(document.querySelector(`#collapse-${moveCount}`));
     collapse.show();
+
+    const a = document.getElementById('something-random-1231231536815368');
+    $(a).on('click', () => {
+        debugger
+        console.log('Hello World');
+    })
 
     specificSortableTable(`#collapse-content-${moveCount} .move-list--draggable`, '.move-list-item--draggable')
 });
