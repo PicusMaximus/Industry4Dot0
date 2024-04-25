a6269b3eb93eeeae56a8ade3f781833b0e1f2b8b
Lukas <tritonow1@gmail.com>
2024-04-25 10:42:33 +0200

    updated sendStatus

--- src/SPS/src/UuidGeneration.py
+++ src/SPS/src/UuidGeneration.py
@@ -4,4 +4,5 @@ def generate_uuid_with_mac_seed(seed):
     mac_address = uuid.getnode()
     namespace = uuid.UUID(int=mac_address)
     uuid_with_mac_seed = uuid.uuid5(namespace, str(seed))
+    print(uuid_with_mac_seed)
     return uuid_with_mac_seed
